a2f85c432172553196d09584daf78cd908e4ff21
Facundo Acosta <facundoacostayl@outlook.com>
2023-02-21 17:31:28 -0300

    Update text in PresentationGroup component

--- src/components/presentation/PresentationGroup.tsx
+++ src/components/presentation/PresentationGroup.tsx
@@ -1,38 +1,37 @@
 //HOOKS
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 //STYLES
-import styles from './PresentationGroup.module.scss';
+import styles from "./PresentationGroup.module.scss";
 
 //ICONS
-import { FontAwesomeIcon, faReact } from '../icon';
+import { FontAwesomeIcon, faReact } from "../icon";
 
 type Props = {
-  theme: string,
-  language: "english" | "espanol"
-}
+  theme: string;
+  language: "english" | "espanol";
+};
 
 export const PresentationGroup: React.FC<Props> = ({ theme, language }) => {
-
   return (
     <ul className={`${theme === "dark" ? styles.dark : styles.light}`}>
-      {language === "english" ?
+      {language === "english" ? (
         <>
           <li className={`${styles.greeting}`}>Hi, i'm</li>
           <li className={`${styles.name}`}>Facundo Acosta</li>
           <div className={`${styles.presentationBox}`}>
-            <p>Full-Stack developer</p>
+            <p>Backend developer</p>
           </div>
         </>
-        :
+      ) : (
         <>
-        <li className={`${styles.greeting}`}>Hola, soy</li>
-        <li className={`${styles.name}`}>Facundo Acosta</li>
-        <div className={`${styles.presentationBox}`}>
-          <p>Desarrollador Full-Stack</p>
-        </div>
-      </>
-      }
+          <li className={`${styles.greeting}`}>Hola, soy</li>
+          <li className={`${styles.name}`}>Facundo Acosta</li>
+          <div className={`${styles.presentationBox}`}>
+            <p>Desarrollador Full-Stack</p>
+          </div>
+        </>
+      )}
     </ul>
-  )
-}
+  );
+};
