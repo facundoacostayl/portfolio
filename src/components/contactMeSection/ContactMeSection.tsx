f19632cc5765e3f369cbaaee1dd62c37ea39ca6d
Facundo Acosta <facundoacostayl@outlook.com>
2023-02-21 18:39:21 -0300

    Update resume download links in ContactMeSection component

--- src/components/contactMeSection/ContactMeSection.tsx
+++ src/components/contactMeSection/ContactMeSection.tsx
@@ -6,7 +6,7 @@ import {
   faDownload,
   faEnvelope,
   faArrowUp,
-  faSquarePhone
+  faSquarePhone,
 } from "../icon";
 import { Theme } from "../../themeContext/types";
 
@@ -44,41 +44,50 @@ export const ContactMeSection = ({ theme, language }: Props) => {
               <h3>You can find me on:</h3>
               <div className={styles.socialMediaBox}>
                 <div className={`${styles.socialBox}`}>
-                <a target="_blank" href="https://www.linkedin.com/in/facundoacostayl">
-                  <i>
-                    <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-                  </i>
-                </a>
-                <a href="mailto:facundoacostayl@gmail.com">
-                  <i>
-                    <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-                  </i>
-                </a>
-                <a target="_blank" href="tel:1133436143">
-                  <i>
-                    <FontAwesomeIcon icon={faSquarePhone}></FontAwesomeIcon>
-                  </i>
-                </a>
-                
-                <a target="_blank" href="https://github.com/facundoacostayl">
-                  <i>
-                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                  </i>
-                </a>
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/facundoacostayl"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                    </i>
+                  </a>
+                  <a href="mailto:facundoacostayl@gmail.com">
+                    <i>
+                      <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                    </i>
+                  </a>
+                  <a target="_blank" href="tel:1133436143">
+                    <i>
+                      <FontAwesomeIcon icon={faSquarePhone}></FontAwesomeIcon>
+                    </i>
+                  </a>
+
+                  <a target="_blank" href="https://github.com/facundoacostayl">
+                    <i>
+                      <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                    </i>
+                  </a>
                 </div>
                 <div className={`${styles.resumeBox}`}>
-                <a target="_blank" href="https://media-exp1.licdn.com/dms/document/C4D2DAQF0ERHVAWyLlA/profile-treasury-document-pdf-analyzed/0/1657501197672?e=1658361600&v=beta&t=Swn7HUi9HTbAItIEpgKoMWZcIQuNpEvDSjaeP-VaPvw">
-                  <i>
-                    <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
-                  </i>
-                  <p>My CV (English)</p>
-                </a>
-                <a target="_blank" href="https://media-exp2.licdn.com/dms/document/C4D2DAQEhLgPF5opMWg/profile-treasury-document-pdf-analyzed/0/1657501152056?e=1658361600&v=beta&t=IHbLCdf7bBn_pUTMrPyHBaO3spg1LQSc7YHasaAXe40">
-                  <i>
-                    <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
-                  </i>
-                  <p>My CV (Spanish)</p>
-                </a>
+                  <a
+                    target="_blank"
+                    href="https://media.licdn.com/dms/document/C4D2DAQGi18kIXeDWVw/profile-treasury-document-pdf-analyzed/0/1677015227763?e=1677715200&v=beta&t=TWxIw8Iofmaguz7rqXe7_aefh1WW2QQkV9VXp8Hy7fI"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
+                    </i>
+                    <p>My CV (English)</p>
+                  </a>
+                  <a
+                    target="_blank"
+                    href="https://media.licdn.com/dms/document/C4D2DAQEAsNopuWTLDA/profile-treasury-document-pdf-analyzed/0/1677015274968?e=1677715200&v=beta&t=56SWHuEF79D7uRadwe2PMS17Il2hVUFjsMml8pBio4g"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
+                    </i>
+                    <p>My CV (Spanish)</p>
+                  </a>
                 </div>
               </div>
               <h4>Thank you for visiting!</h4>
@@ -115,41 +124,50 @@ export const ContactMeSection = ({ theme, language }: Props) => {
             <div className={styles.contact}>
               <h3>Estoy en:</h3>
               <div className={styles.socialMediaBox}>
-              <div className={`${styles.socialBox}`}>
-                <a target="_blank" href="https://www.linkedin.com/in/facundoacostayl">
-                  <i>
-                    <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-                  </i>
-                </a>
-                <a href="mailto:facundoacostayl@gmail.com">
-                  <i>
-                    <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-                  </i>
-                </a>
-                <a target="_blank" href="tel:1133436143">
-                  <i>
-                    <FontAwesomeIcon icon={faSquarePhone}></FontAwesomeIcon>
-                  </i>
-                </a>
-                <a target="_blank" href="https://github.com/facundoacostayl">
-                  <i>
-                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                  </i>
-                </a>
+                <div className={`${styles.socialBox}`}>
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/facundoacostayl"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                    </i>
+                  </a>
+                  <a href="mailto:facundoacostayl@gmail.com">
+                    <i>
+                      <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                    </i>
+                  </a>
+                  <a target="_blank" href="tel:1133436143">
+                    <i>
+                      <FontAwesomeIcon icon={faSquarePhone}></FontAwesomeIcon>
+                    </i>
+                  </a>
+                  <a target="_blank" href="https://github.com/facundoacostayl">
+                    <i>
+                      <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                    </i>
+                  </a>
                 </div>
                 <div className={`${styles.resumeBox}`}>
-                <a target="_blank" href="https://media-exp1.licdn.com/dms/document/C4D2DAQF0ERHVAWyLlA/profile-treasury-document-pdf-analyzed/0/1657501197672?e=1658361600&v=beta&t=Swn7HUi9HTbAItIEpgKoMWZcIQuNpEvDSjaeP-VaPvw">
-                  <i>
-                    <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
-                  </i>
-                  <p>Mi CV (Inglés)</p>
-                </a>
-                <a target="_blank" href="https://media-exp2.licdn.com/dms/document/C4D2DAQEhLgPF5opMWg/profile-treasury-document-pdf-analyzed/0/1657501152056?e=1658361600&v=beta&t=IHbLCdf7bBn_pUTMrPyHBaO3spg1LQSc7YHasaAXe40">
-                  <i>
-                    <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
-                  </i>
-                  <p>Mi CV (Español)</p>
-                </a>
+                  <a
+                    target="_blank"
+                    href="https://media.licdn.com/dms/document/C4D2DAQGi18kIXeDWVw/profile-treasury-document-pdf-analyzed/0/1677015227763?e=1677715200&v=beta&t=TWxIw8Iofmaguz7rqXe7_aefh1WW2QQkV9VXp8Hy7fI"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
+                    </i>
+                    <p>Mi CV (Inglés)</p>
+                  </a>
+                  <a
+                    target="_blank"
+                    href="https://media.licdn.com/dms/document/C4D2DAQEAsNopuWTLDA/profile-treasury-document-pdf-analyzed/0/1677015274968?e=1677715200&v=beta&t=56SWHuEF79D7uRadwe2PMS17Il2hVUFjsMml8pBio4g"
+                  >
+                    <i>
+                      <FontAwesomeIcon icon={faDownload}></FontAwesomeIcon>
+                    </i>
+                    <p>Mi CV (Español)</p>
+                  </a>
                 </div>
               </div>
               <h4>Gracias por la visita!</h4>
