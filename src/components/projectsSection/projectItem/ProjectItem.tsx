96572ac79d831873b129bc0c5176465fb7d27e1f
facundoacostayl <facundoacostayl@outlook.com>
2024-05-13 07:54:07 -0300

    update ProjectItem logos

--- src/components/projectsSection/projectItem/ProjectItem.tsx
+++ src/components/projectsSection/projectItem/ProjectItem.tsx
@@ -53,19 +53,20 @@ export const ProjectItem: React.FC<Props> = ({
               />
               <p>NodeJS</p>
             </div>
-            <div className={`${styles.icon}`}>
-              <img
-                src="https://cdn.pixabay.com/photo/2020/03/17/17/46/database-4941338_960_720.png"
-                alt="postgres-logo"
-              />
-              <p>PostgreSQL</p>
-            </div>
+
             <div className={`${styles.icon}`}>
               <img
                 src="https://img.stackshare.io/service/7419/20165699.png"
                 alt="type-orm"
               />
               <p>TypeORM</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://cdn.pixabay.com/photo/2020/03/17/17/46/database-4941338_960_720.png"
+                alt="postgres-logo"
+              />
+              <p>PostgreSQL</p>
             </div>
             <div className={`${styles.icon}`}>
               <img
@@ -89,32 +90,6 @@ export const ProjectItem: React.FC<Props> = ({
               <p>Tailwind</p>
             </div>
           </div>
-          <div
-            className={`${theme === "dark" ? styles.dark : styles.light} ${
-              styles.info
-            }`}
-          >
-            <a
-              target="_blank"
-              href="https://edendesk-crm-frontend.vercel.app/"
-              className={`${styles.icon}`}
-            >
-              <i>
-                <FontAwesomeIcon icon={faLink} />
-              </i>
-              <p>Website</p>
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/facundoacostayl/edendeskCRM-backend"
-              className={`${styles.icon}`}
-            >
-              <i>
-                <FontAwesomeIcon icon={faGithub} />
-              </i>
-              <p>Github Repo</p>
-            </a>
-          </div>
         </>
       )}
       {project === "proviamo" && (
@@ -122,63 +97,86 @@ export const ProjectItem: React.FC<Props> = ({
           <div className={`${styles.stack}`}>
             <div className={`${styles.icon}`}>
               <img
-                src="https://i.ibb.co/ZHPxSf1/80-803501-javascript-logo-logo-de-java-script-png.png"
-                alt="-javascript-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/b/bb/Java-logo.png"
+                alt="java-logo"
+              />
+              <p>Java</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png"
+                alt="springboot-logo"
+              />
+              <p>SpringBoot</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://cdn.pixabay.com/photo/2020/03/17/17/46/database-4941338_960_720.png"
+                alt="postgres-logo"
+              />
+              <p>PostgreSQL</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://img.icons8.com/color/480/java-web-token.png"
+                alt="type-orm"
+              />
+              <p>JWT</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+                alt="react-logo"
+              />
+              <p>React</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/919/919832.png"
+                alt="typescript-logo"
+              />
+              <p>Typescript</p>
+            </div>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png"
+                alt="tailwind-logo"
+              />
+              <p>Tailwind</p>
+            </div>
+          </div>
+        </>
+      )}
+
+      {project === "multipar" && (
+        <>
+          <div className={`${styles.stack}`}>
+            <div className={`${styles.icon}`}>
+              <img
+                src="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png"
+                alt="javascript-logo"
               />
               <p>Javascript</p>
             </div>
             <div className={`${styles.icon}`}>
               <img
-                src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
-                alt="node-logo"
+                src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png"
+                alt="html-logo"
               />
-              <p>NodeJS</p>
+              <p>HTML</p>
             </div>
             <div className={`${styles.icon}`}>
               <img
-                src="https://cdn-icons-png.flaticon.com/512/2965/2965327.png"
-                alt="sheets-logo"
+                src="https://raw.githubusercontent.com/johan-v-r/LibSassBuilder/main/package/sass.png"
+                alt="sass-logo"
               />
-              <p>Google Sheets</p>
+              <p>Sass</p>
             </div>
-            <div className={`${styles.icon}`}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png"
-                alt="css-logo"
-              />
-              <p>CSS</p>
-            </div>
-          </div>
-          <div
-            className={`${theme === "dark" ? styles.dark : styles.light} ${
-              styles.info
-            }`}
-          >
-            <a
-              target="_blank"
-              href="https://proviamoalmacengourmet.com"
-              className={`${styles.icon}`}
-            >
-              <i>
-                <FontAwesomeIcon icon={faLink} />
-              </i>
-              <p>Website</p>
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/facundoacostayl/Proviamo-E-Commerce"
-              className={`${styles.icon}`}
-            >
-              <i>
-                <FontAwesomeIcon icon={faGithub} />
-              </i>
-              <p>Github Repo</p>
-            </a>
           </div>
         </>
       )}
 
-      {project === "marketlist" && (
+      {project === "autoprices" && (
         <>
           <div className={`${styles.stack}`}>
             <div className={`${styles.icon}`}>
@@ -230,24 +228,6 @@ export const ProjectItem: React.FC<Props> = ({
               />
               <p>Sass</p>
             </div>
-          </div>
-          <div
-            className={`${theme === "dark" ? styles.dark : styles.light} ${
-              styles.info
-            }`}
-          >
-            <a target="_blank" href="#" className={`${styles.icon}`}>
-              <i>
-                <FontAwesomeIcon icon={faLink} />
-              </i>
-              <p>Website</p>
-            </a>
-            <a target="_blank" href="#" className={`${styles.icon}`}>
-              <i>
-                <FontAwesomeIcon icon={faGithub} />
-              </i>
-              <p>Github Repo</p>
-            </a>
           </div>
         </>
       )}
